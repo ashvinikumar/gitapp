0222f990038aebb9417d07cd6b730997cc68bd70
ashvinikumar <ashvinikumar45@gmail.com>
2019-06-06 22:03:31 +0530

    profile data API integration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-
+import axios from "axios";
 import LeftColumn from "./LeftColumn/LeftColumn";
 import RightColumn from "./RightColumn/RightColumn";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="row padding5">
-        <div className="container">
-          {/* left column */}
-          <div className="left-column">
-            <LeftColumn />
-          </div>
-          {/* right column */}
-          <div className="right-column">
-            <RightColumn />
+class App extends Component {
+
+  state = {
+    profileData: {}
+  }
+
+  componentDidMount() {
+    this.getProfileData();
+  }
+
+  getProfileData = () => {
+    axios
+      .get("https://api.github.com/users/supreetsingh247")
+      .then(response => {
+        this.setState({
+          profileData: response.data
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="row padding5">
+          <div className="container">
+            {/* left column */}
+            <div className="left-column">
+              <LeftColumn profileData={this.state.profileData} />
+            </div>
+            {/* right column */}
+            <div className="right-column">
+              <RightColumn />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/LeftColumn/LeftColumn.js
+++ src/LeftColumn/LeftColumn.js
@@ -1,20 +1,21 @@
 import React from "react";
 import "./LeftColumn.css";
 
-const LeftColumn = () => {
+const LeftColumn = props => {
+  console.log(props);
   return (
     <React.Fragment>
       <div className="profile">
         <img
           className="avatar"
-          src="https://avatars3.githubusercontent.com/u/7427552?s=460&v=4"
+          src={props.profileData.avatar_url}
           alt="avatar"
         />
       </div>
       <div className="profile-details">
         <h1 className="vcard-names">
-          <p className="vcard-fullname">Supreet Singh</p>
-          <p className="vcard-username">supreetsingh247</p>
+          <p className="vcard-fullname">{props.profileData.name}</p>
+          <p className="vcard-username">{props.profileData.login}</p>
         </h1>
       </div>
       <div className="edit-profile-area">
@@ -25,15 +26,17 @@ const LeftColumn = () => {
         </div>
       </div>
       <div className="profile-details">
-        <p>Front end developer since 1.5 years</p>
+        <p>{props.profileData.bio}</p>
 
         <div className="border-top">
-          <p>Target Corporation</p>
-          <p>India</p>
+          <p>{props.profileData.company}</p>
+          <p>{props.profileData.location}</p>
           <p>
-            <a href="mailto:supreetsingh.247@gmail.com">
-              mailto:supreetsingh.247@gmail.com
-            </a>
+            {props.profileData.email ? (
+              <a href={`mailto: ${props.profileData.email}`}>
+                mailto:{props.profileData.email}
+              </a>
+            ) : null}
           </p>
         </div>
       </div>
